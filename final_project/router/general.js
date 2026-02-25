66b00bf959c0150a882b202d6718fa843f11a492
Pratik Sharma <pratik@Pratiks-MacBook-Pro.local>
2026-02-25 19:12:09 +0545

    Implemented try catch and error handling

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -39,66 +39,86 @@ public_users.post("/register", (req, res) => {
 
 // Get the book list available in the shop
 public_users.get('/', function (req, res) {
-  const get_books = new Promise((resolve, reject) => {
-    resolve(books);
-  });
-  get_books.then((boks) => {
-    res.send(JSON.stringify(boks, null, 4));
-  });
+  try {
+    const get_books = new Promise((resolve, reject) => {
+      resolve(books);
+    });
+    get_books.then((boks) => {
+      res.send(JSON.stringify(boks, null, 4));
+    });
+  } catch (error) {
+    res.status(500).send("Internal Server Error");
+  }
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn', function (req, res) {
-  const get_book = new Promise((resolve, reject) => {
-    resolve(books[req.params.isbn]);
-  });
-  get_book.then((book) => {
-    res.send(book);
-  });
+  try {
+    const get_book = new Promise((resolve, reject) => {
+      resolve(books[req.params.isbn]);
+    });
+    get_book.then((book) => {
+      res.send(book);
+    });
+  } catch (error) {
+    res.status(500).send("Internal Server Error");
+  }
 });
 
 // Get book details based on author
 public_users.get('/author/:author', function (req, res) {
   const author = req.params.author;
-  const getBooksByAuthor = new Promise((resolve, reject) => {
-    const keys = Object.keys(books);
-    const result = [];
-    for (let i = 0; i < keys.length; i++) {
-      const book = books[keys[i]];
-      if (book.author === author) {
-        result.push(book);
+  try {
+    const getBooksByAuthor = new Promise((resolve, reject) => {
+      const keys = Object.keys(books);
+      const result = [];
+      for (let i = 0; i < keys.length; i++) {
+        const book = books[keys[i]];
+        if (book.author === author) {
+          result.push(book);
+        }
       }
-    }
-    resolve(result);
-  });
-  getBooksByAuthor.then((result) => {
-    res.send(JSON.stringify(result, null, 4));
-  });
+      resolve(result);
+    });
+    getBooksByAuthor.then((result) => {
+      res.send(JSON.stringify(result, null, 4));
+    });
+  } catch (error) {
+    res.status(500).send("Internal Server Error");
+  }
 });
 
 // Get all books based on title
 public_users.get('/title/:title', function (req, res) {
   const title = req.params.title;
-  const getBooksByTitle = new Promise((resolve, reject) => {
-    const keys = Object.keys(books);
-    const result = [];
-    for (let i = 0; i < keys.length; i++) {
-      const book = books[keys[i]];
-      if (book.title === title) {
-        result.push(book);
+  try {
+    const getBooksByTitle = new Promise((resolve, reject) => {
+      const keys = Object.keys(books);
+      const result = [];
+      for (let i = 0; i < keys.length; i++) {
+        const book = books[keys[i]];
+        if (book.title === title) {
+          result.push(book);
+        }
       }
-    }
-    resolve(result);
-  });
-  getBooksByTitle.then((result) => {
-    res.send(JSON.stringify(result, null, 4));
-  });
+      resolve(result);
+    });
+    getBooksByTitle.then((result) => {
+      res.send(JSON.stringify(result, null, 4));
+    });
+  } catch (error) {
+    res.status(500).send("Internal Server Error");
+  }
 });
 
 //  Get book review
 public_users.get('/review/:isbn', function (req, res) {
   const isbn = req.params.isbn;
-  res.send(books[isbn].reviews);
+  if (books[isbn]) {
+    res.send(books[isbn].reviews);
+  } else {
+    res.status(404).json({ message: "Book not found" });
+  }
 });
 
 module.exports.general = public_users;
